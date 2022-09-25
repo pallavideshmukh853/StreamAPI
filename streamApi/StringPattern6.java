fbbe596918cc8d2fff9e7d3b9a1addb527ca60a0
pallavideshmukh853 <112393954+pallavideshmukh853@users.noreply.github.com>
2022-09-25 23:23:03 +0530

    Add files via upload

---
+++ streamApi/StringPattern6.java
@@ -0,0 +1,20 @@
+package streamApi;
+
+import java.util.Scanner;
+
+public class StringPattern6 {
+
+	public static void main(String[] args) {
+		
+	
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter String Pattern");
+	String s=scan.next();
+	
+	for(int i=s.length()-1;i>=s.length()/2;i--) {
+	System.out.print(s.charAt(i));
+	
+	}
+	
+}
+}
